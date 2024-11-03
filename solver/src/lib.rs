b14f670391a296ec6837edbc0c57b0559de6385e
Sanjeet Dasharath <sanjeetdasharath@gmail.com>
2024-11-03 15:06:31 +1100

    improve nogoods optimization even more

--- solver/src/lib.rs
+++ solver/src/lib.rs
@@ -47,9 +47,8 @@ impl NoGoods {
 
         for idx in solution {
             current = current.children.entry(idx).or_insert(TrieNode::new());
+            current.is_leaf = true;
         }
-
-        current.is_leaf = true;
     }
 
     pub fn search(&self, mut solution: Vec<usize>) -> bool {
